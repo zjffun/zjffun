88e207a22079f7dce53a67ff4d384e2439bd65f2
zjf <zjffun@gmail.com>
2019-06-02 21:09:14 +0800

    Fix blogs' styles

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import './Blogs.scss';
+import styles from './Blogs.scss';
 import { extractPages } from '../../utilities/content-utils';
 
 export default props => {
   const { sections, section } = props;
   const pages = extractPages(section);
   return (
-    <main className="blogs">
-      <article className="blogs__article">
+    <main className={styles.blogs}>
+      <article className={styles.blogs__article}>
         <ul>
           {pages.map(d => (
             <li key={d.path}>
               <a href={d.url}>
-                <div className="blog">
-                  <h3 className="blog__title">{d.name}</h3>
-                  <p className="blog__digest">
+                <div className={styles.blog}>
+                  <h3 className={styles.blog__title}>{d.name}</h3>
+                  <p className={styles.blog__digest}>
                     {d.digest || '这个人太懒了，咩有写摘要'}
                   </p>
-                  <p className="blog__info">
-                    <span className="blog__post-tiem">{d.postTime}</span>
+                  <p className={styles.blog__info}>
+                    <span className={styles['blog__post-tiem']}>
+                      {d.postTime}
+                    </span>
                     阅读时间：
-                    <span className="blog__reading-time">{d.size}</span>
+                    <span className={styles['blog__reading-time']}>
+                      {d.size}
+                    </span>
                   </p>
                 </div>
               </a>
@@ -28,16 +32,17 @@ export default props => {
           ))}
         </ul>
       </article>
-      <aside className="blogs__aside">
-        <section className="section">
-          <p className="section__title">分类</p>
-          <ul className="items">
+      <aside className={styles.blogs__aside}>
+        <section className={styles.section}>
+          <p className={styles.section__title}>分类</p>
+          <ul className={styles.items}>
             {sections.map(d => (
               <li
                 key={d.path}
-                className={`items__item ${
-                  d.name === section.name ? 'items__item--active' : ''
-                }`}
+                className={[
+                  styles.items__item,
+                  d.name === section.name ? styles['items__item--active'] : ''
+                ]}
               >
                 <a href={d.url}>{d.name}</a>
               </li>
