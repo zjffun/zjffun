33af680d0c8f4dde240350854f5623017d0c5868
zhangjufeng <zjffun@gmail.com>
2020-05-20 20:26:59 +0800

    add lints, separate note module

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,30 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/js/index.js",
-    note: "./src/js/note.js"
+    index: './src/js/index.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "publish/index.html"),
-      chunks: ["index"]
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'publish/index.html'),
+      chunks: ['index'],
     }),
-    new HtmlWebpackPlugin({
-      filename: "note.html",
-      template: path.resolve(__dirname, "publish/note.html"),
-      chunks: ["note"]
-    })
   ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000
-  }
 };
