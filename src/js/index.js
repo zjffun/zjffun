b1df5eae85cf03090ab5f3ab2484f809fa176621
Jufeng Zhang <zjffun@gmail.com>
2025-04-12 17:23:11 +0800

    feat: update content

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,1 @@
 import '../scss/index.scss';
-
-const faviconEl = document.createElement('link');
-const faviconIndex = Math.floor(Math.random() * 2);
-faviconEl.rel = 'icon';
-faviconEl.href = `./favicon/${faviconIndex}.ico`;
-document.head.insertAdjacentElement('beforeend', faviconEl);
